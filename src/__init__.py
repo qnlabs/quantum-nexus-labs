4ff8ffa002c1df229e71704643065f539059cd39
qnlabs <quantumnexuslabsqnl@gmail.com>
2025-01-02 14:56:44 +0100

    Add root package initialization

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+Quantum Nexus Labs
+Main package initialization
+"""
+
+from . import quantum
+from . import aria
+from . import protocols
+
+__version__ = '1.0.0'
+__all__ = ['quantum', 'aria', 'protocols']
+
+# Package metadata
+PACKAGE_NAME = "quantum-nexus-labs"
+AUTHOR = "Quantum Nexus Labs"
+DESCRIPTION = "Advanced Quantum Computing & AI Research"
