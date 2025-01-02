1b050e135f724782c1aa600aa8512e4236826c0f
qnlabs <quantumnexuslabsqnl@gmail.com>
2025-01-02 17:10:53 +0100

    Test basic page setup

---
+++ app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl">Test</h1>
+    </div>
+  );
+}
